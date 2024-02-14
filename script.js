1cbf52661d104432f481eca17bb19dd602b32348
OwnzoneCrow <150837793+OwnzoneCrow@users.noreply.github.com>
2024-02-15 02:31:24 +0900

    Add javascript and computer choice function

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("RPS")
+//  RPS Variable, why does it have to be a let, and not a const //
+let choices = ['rock', 'paper', 'scissors']
+
+// function to pull the computer's choice; this seems kind of weird but I assume this has to do with arrays at the end?//
+
+function getComputerChoice(choices){
+
+return choices[Math.floor (Math.random() * choices.length)];
+
+}
+console.log(getComputerChoice(choices))
